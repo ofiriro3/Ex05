d0ebb80bb0695d92faa0eacb77ceca4ff367b9fe
DESKTOP-T5UJ4HK\ofiriro3 <ofiriro3@gmail.com>
2017-06-29 11:44:39 +0300

    my first commits

--- B17 Ex05 OfirLapid 305260846 AsafFried 314078676/Ex05.WinFormUI/FormColorChoice.cs
+++ B17 Ex05 OfirLapid 305260846 AsafFried 314078676/Ex05.WinFormUI/FormColorChoice.cs
@@ -7,17 +7,31 @@ namespace Ex05.WinFormUI
 {
     public class FormColorChoice : Form
     {
-
+        Dictionary<String,Button> colorButtons; 
 
         public FormColorChoice()
         {
             this.Text = "Pick A Color:";
             this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
-            this.Size = new System.Drawing.Size(150, 150);
+            this.Size = new System.Drawing.Size(300, 150);
+            
+        }
 
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            InitControls();
+        }
+
+        private void InitControls()
+        {
+            createColoredButton("Blue");
 
         }
 
-
+        private void createColoredButton(string color)
+        {
+            
+        }
     }
 }
--- B17 Ex05 OfirLapid 305260846 AsafFried 314078676/Ex05.WinFormUI/FormLogin.cs
+++ B17 Ex05 OfirLapid 305260846 AsafFried 314078676/Ex05.WinFormUI/FormLogin.cs
@@ -1,10 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
 
 namespace Ex05.WinFormUI
 {
-    class FormLogin
+    public class FormLogin : Form
     {
+        private readonly int r_MinNumberOfChances;
+        private readonly int r_MaxNumberOfChances;
+        private int m_SelectedNumberOfChances;
+        Button m_ButtonStart = new Button();
+        Button m_ButtonNumberOfChances = new Button();
+
+        public FormLogin()
+        {
+            this.Size = new Size(250, 150);
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "Bool Pgia";
+            r_MinNumberOfChances = 4;
+            r_MaxNumberOfChances = 10;
+            m_SelectedNumberOfChances = 4;
+        }
+
+        public int SelectedNumberOfChances
+        {
+            get { return m_SelectedNumberOfChances; }
+            set { m_SelectedNumberOfChances = value; }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            InitControls();
+        }
+
+        private void InitControls()
+        {
+            InitButtonNumberOfChances();
+            InitButtonStart();
+        }
+
+        private void InitButtonNumberOfChances()
+        {
+            m_ButtonNumberOfChances.Text = string.Format("Number of chances: {0}", m_SelectedNumberOfChances);
+            m_ButtonStart.Width = this.Width - 10;
+            this.m_ButtonNumberOfChances.Click += new EventHandler(m_ButtonNumberOfChances_Click);
+        }
+
+        private void InitButtonStart()
+        {
+            m_ButtonStart.Text = "Start";
+            this.m_ButtonStart.Click += new EventHandler(m_ButtonStart_Click);
+        }
+
+        void m_ButtonNumberOfChances_Click(object sender, EventArgs e)
+        {
+            m_SelectedNumberOfChances++;
+            m_ButtonNumberOfChances.Text = string.Format("Number of chances: {0}", m_SelectedNumberOfChances);
+        }
+
+        void m_ButtonStart_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
     }
 }
