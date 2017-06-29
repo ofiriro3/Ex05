dc0d777e7426b508f3dfdbc0ba622632b19a4154
DESKTOP-T5UJ4HK\ofiriro3 <ofiriro3@gmail.com>
2017-06-29 12:06:49 +0300

    COLORSt

--- B17 Ex05 OfirLapid 305260846 AsafFried 314078676/Ex05.WinFormUI/FormColorChoice.cs
+++ B17 Ex05 OfirLapid 305260846 AsafFried 314078676/Ex05.WinFormUI/FormColorChoice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
@@ -7,14 +8,14 @@ namespace Ex05.WinFormUI
 {
     public class FormColorChoice : Form
     {
-        Dictionary<String,Button> colorButtons; 
+        Dictionary<String,Button> m_ColorButtons; 
 
         public FormColorChoice()
         {
             this.Text = "Pick A Color:";
             this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
             this.Size = new System.Drawing.Size(300, 150);
-            
+            m_ColorButtons = new Dictionary<string, Button>();
         }
 
         protected override void OnLoad(EventArgs e)
@@ -25,13 +26,25 @@ namespace Ex05.WinFormUI
 
         private void InitControls()
         {
-            createColoredButton("Blue");
-
+            createColoredButton(Color.Purple);
+            createColoredButton(Color.Red);
+            createColoredButton(Color.Green);
+            createColoredButton(Color.WhiteSmoke);
+            createColoredButton(Color.Blue);
+            createColoredButton(Color.Yellow);
+            createColoredButton(Color.Brown);
+            createColoredButton(Color.White);
+            foreach(Button button in m_ColorButtons.Values)
+            {
+                this.Controls.Add(button);
+            }
         }
 
-        private void createColoredButton(string color)
+        private void createColoredButton(Color i_Color)
         {
-            
+            Button coloredButton = new Button();
+            coloredButton.BackColor = i_Color;
+            m_ColorButtons.Add(i_Color.ToString(), coloredButton);
         }
     }
 }
